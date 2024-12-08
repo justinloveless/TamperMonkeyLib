dd39512b1fbbe7b2d29271b46c3cf2328b5b71ca
Justin Loveless <justin.loveless@hp.com>
2024-12-08 15:31:36 -0600

    import other file

--- RemoveDubs/dependency.js
+++ RemoveDubs/dependency.js
@@ -1,3 +1,5 @@
-function logStuff(){
+async function logStuff(){
     console.log("This is a log message");
+    const removeDubs = await import ("https://raw.githubusercontent.com/justinloveless/TamperMonkeyLib/main/RemoveDubs/Remove Dubs.js");
+    removeDubs.toggleDubs();
 }
