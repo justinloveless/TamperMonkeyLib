39e87485dae566bbbbdb7a9beafb8711db15729a
Justin Loveless <justin.loveless@hp.com>
2024-12-08 14:55:23 -0600

    Separate toggleDubs();

---
+++ RemoveDubs/Remove Dubs.js
@@ -0,0 +1,30 @@
+function toggleDubs() {
+    const checkbox = document.createElement("input");
+    const textNode = document.createTextNode("Show Dubs");
+    checkbox.id = "dubs";
+    checkbox.type="checkbox";
+    checkbox.checked = GM_getValue("show_dubs");
+    checkbox.addEventListener('change', (event) => {
+        GM_setValue("show_dubs", event.currentTarget.checked);
+        location.reload();
+    })
+
+    const label = document.createElement("label");
+    label.appendChild(checkbox);
+    label.appendChild(textNode);
+    const form = document.getElementById("filter_toggle_form");
+    form.appendChild(label);
+
+    if (!checkbox.checked) {
+        const days = document.getElementsByClassName("releases");
+        for (var i = 0; i < days.length; i++){
+
+            var items = days[i].children;
+            for (var j = 0; j < items.length; j++){
+                if (days[i].children[j].innerHTML.includes("Dub)")){
+                    days[i].children[j].innerHTML = "";
+                }
+            }
+        }
+    }
+}
--- RemoveDubs/Remove Dubs.user.js
+++ RemoveDubs/Remove Dubs.user.js
@@ -10,39 +10,11 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=crunchyroll.com
 // @grant        GM_getValue
 // @grant        GM_setValue
-// @require      https://raw.githubusercontent.com/justinloveless/TamperMonkeyLib/main/RemoveDubs/Remove%20Dubs.user.js
+// @require      https://raw.githubusercontent.com/justinloveless/TamperMonkeyLib/main/RemoveDubs/Remove%20Dubs.js
 
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    const checkbox = document.createElement("input");
-    const textNode = document.createTextNode("Show Dubs");
-    checkbox.id = "dubs";
-    checkbox.type="checkbox";
-    checkbox.checked = GM_getValue("show_dubs");
-    checkbox.addEventListener('change', (event) => {
-        GM_setValue("show_dubs", event.currentTarget.checked);
-        location.reload();
-    })
-
-    const label = document.createElement("label");
-    label.appendChild(checkbox);
-    label.appendChild(textNode);
-    const form = document.getElementById("filter_toggle_form");
-    form.appendChild(label);
-
-    if (!checkbox.checked) {
-        const days = document.getElementsByClassName("releases");
-        for (var i = 0; i < days.length; i++){
-
-            var items = days[i].children;
-            for (var j = 0; j < items.length; j++){
-                if (days[i].children[j].innerHTML.includes("Dub)")){
-                    days[i].children[j].innerHTML = "";
-                }
-            }
-        }
-    }
+    toggleDubs();
 })();
