45870f622eefa7da8becc898906b9edda57c7124
Justin Loveless <justin.loveless@hp.com>
2024-12-08 15:07:25 -0600

    use dynamic import

--- RemoveDubs/Remove Dubs.user.js
+++ RemoveDubs/Remove Dubs.user.js
@@ -14,7 +14,10 @@
 
 // ==/UserScript==
 
+const RemoveDubs = await import('https://raw.githubusercontent.com/justinloveless/TamperMonkeyLib/main/RemoveDubs/Remove%20Dubs.mjs');
+
 (function() {
     'use strict';
-    toggleDubs();
+    console.log("RemoveDubs", RemoveDubs);
+    RemoveDubs.toggleDubs();
 })();
