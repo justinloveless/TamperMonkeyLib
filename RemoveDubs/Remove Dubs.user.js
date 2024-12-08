61e8b923496a349dcfdf965810c2d47478b4f885
Justin Loveless <justin.loveless@hp.com>
2024-12-08 15:41:46 -0600

    update version

--- RemoveDubs/Remove Dubs.user.js
+++ RemoveDubs/Remove Dubs.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Remove Dubs
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.crunchyroll.com/simulcastcalendar
