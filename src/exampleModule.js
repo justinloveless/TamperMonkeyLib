bb004c8e06727ad7f8b3f3893c0a95944cc380f7
Justin Loveless <justin.loveless@hp.com>
2024-12-08 21:11:51 -0600

    prepare for templating

---
+++ src/exampleModule.js
@@ -0,0 +1,3 @@
+export function demo(message){
+    console.log("This is a demo function from another file.", message);
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
-// ==UserScript==
-// @name         Your Script Name
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Your description here
-// @author       You
-// @match        *://*/*
-// @grant        GM_getValue
-// @grant        GM_setValue
-// ==/UserScript==
-
-import { toggleDubs } from './Remove Dubs';
+import { demo } from './exampleModule';
 
 console.log('Tampermonkey Script Started (using webpack)');
-toggleDubs();
+demo('calling demo function from main.js');
