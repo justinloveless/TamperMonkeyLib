783c49d7b0e82abb8d50829e29d50fc499552359
Justin Loveless <justin.loveless@hp.com>
2024-12-08 21:37:28 -0600

    fix merge conflicts [skip ci]

--- dist/script.meta.js
+++ dist/script.meta.js
@@ -1,13 +1,7 @@
 // ==UserScript==
-<<<<<<< HEAD
 // @name My Userscript
 // @description A cool userscript
 // @version 1.0.0
-=======
-// @name Your Script Name
-// @description Your description here
-// @version 1.0.6
->>>>>>> 6f8da43e28eb624a152de41b9f5eed91b270e431
 // @author Your Name
 // @match https://example.com/*
 // @grant GM_getValue
