c9f299fba32d44777fd0d3d8844c72676ce90343
Justin Loveless <justin.loveless@hp.com>
2024-12-08 19:59:25 -0600

    use mjs

--- RemoveDubs/dependency.js
+++ RemoveDubs/dependency.js
@@ -1,5 +1,5 @@
 async function logStuff(){
     console.log("This is a log message");
-    const removeDubs = await import("https://raw.githubusercontent.com/justinloveless/TamperMonkeyLib/main/RemoveDubs/Remove Dubs.js");
+    const removeDubs = await import("https://raw.githubusercontent.com/justinloveless/TamperMonkeyLib/main/RemoveDubs/Remove Dubs.mjs");
     removeDubs.toggleDubs();
 }
